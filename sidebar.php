1005959c1c13fb8c3b694ef7cbb803da5ddf5bf6
Kauan Scheidt <scheidtkauan8@gmail.com>
2023-11-15 22:10:54 -0300

    Ajustes de Tutorial

--- sidebar.php
+++ sidebar.php
@@ -1,54 +1,131 @@
-<div class="row">
-    <h3>Lista de Tutoriais</h3>
+
   <div class="col-4">
-    <nav id="navbar-example3" class="h-100 flex-column align-items-stretch pe-4 border-end">
+    <nav id="navbar-example3" class="h-100 flex-column align-items-stretch pe-4 border-end bg-dark">
       <nav class="nav nav-pills flex-column">
-        <a class="nav-link" href="#item-1"></a>
+        <a class="nav-link ms-3 my-1" href="#item-1">Movimentação (na Web)</a>
         <nav class="nav nav-pills flex-column">
-          <a class="nav-link ms-3 my-1" href="#item-1-1">Item 1-1</a>
-          <a class="nav-link ms-3 my-1" href="#item-1-2">Item 1-2</a>
+          <a class="nav-link ms-3 my-1" href="#item-1-1">Movimento por setas</a>
+          <a class="nav-link ms-3 my-1" href="#item-1-2">Movimentação W,A,S,D</a>
         </nav>
-        <a class="nav-link" href="#item-2">Item 2</a>
-        <a class="nav-link" href="#item-3">Item 3</a>
+        <a class="nav-link ms-3 my-1" href="#item-2">Movimentação (em Aplicativos Móveis)</a>
+
+        <a class="nav-link ms-3 my-1" href="#item-3">Rotação - Giro (na Web)</a>
+
+        <a class="nav-link ms-3 my-1" href="#item-4">Rotação - Giro (em Aplicativos Móveis)</a>
         <nav class="nav nav-pills flex-column">
-          <a class="nav-link ms-3 my-1" href="#item-3-1">Item 3-1</a>
-          <a class="nav-link ms-3 my-1" href="#item-3-2">Item 3-2</a>
+          <a class="nav-link ms-3 my-1" href="#item-4-1">Rotação em R.V (Realidade Virtual)</a>
+          <a class="nav-link ms-3 my-1" href="#item-4-2">Rotação em R.A (Realidade Aumentada) com giroscópio ligado</a>
+          <a class="nav-link ms-3 my-1" href="#item-4-3">Rotação em R.A (Realidade Aumentada) com giroscópio ligado</a>
         </nav>
       </nav>
     </nav>
   </div>
   
-  <div class="col-2"></div>
   
-  <div class="col-6">
+  <div class="col-8">
+  <div class="row">
+    <div class="col-3"></div>
+    <div class="col-6">
+        <img class="centro" src="imagens/logo.png" alt="Logo da Biovirtual" width="100%">
+        <h1 class="centro">Lista de Tutoriais</h1>
+    </div>
+    <div class="col-3"></div>
+    </div>
+
     <div data-bs-spy="scroll" data-bs-target="#navbar-example3" data-bs-smooth-scroll="true" class="scrollspy-example-2" tabindex="0">
       <div id="item-1">
         <h3>Movimentação no Ambiente Virtual</h3>
-        <h4>Conjunto de Teclas</h4>
+        <h4>Para Usos de Desktop ou Web</h4>
+        <h4>Ocorre Através do Conjunto de Teclas para o modo R.A, considerando o giroscópio (des)ligado</h4>
+        
       </div>
+      <br>
       <div id="item-1-1">
-        <h5>A movimentação pode ocorrer pelo conjunto de setas</h5>
-        <h4>...</h4>
+        <div class="row">
+          <div class="col-4">
+            <h5>A movimentação pode ocorrer pelo conjunto de setas</h5>
+          </div>
+          <div class="col-8">
+            <img src="imagens/tutorial/setas.png" alt="Cena do CoSpaces" width="100%">
+          </div>
+        </div>
       </div>
       <div id="item-1-2">
-        <h5>A movimentação pode ocorrer pelo conjunto W, A, S, D</h5>
-        <h4>...</h4>
+        <div class="row">
+          <div class="col-4">
+            <h5>A movimentação pode ocorrer pelo conjunto W, A, S, D</h5>
+          </div>
+          <div class="col-8">
+            <img src="imagens/tutorial/wasd.png" alt="Cena do CoSpaces" width="100%">
+          </div>
+        </div>
       </div>
-      <div id="item-2">
-        <h3>Item 2</h3>
-        <h4>...</h4>
+      <div class="item-2">
+        <h3>Movimentação no Ambiente Virtual</h3>
+        <h4>Para Usos de Aplicativos Móveis</h4>
+        <div class="row">
+          <div class="col-4">
+            <h5>Movimentos de para frente/trás e Saltos são realizados pelos 
+              respectivos ícones representados e destacados em azul na tela</h5>
+          </div>
+          <div class="col-8">
+            <img src="imagens/tutorial/movimento_mobile.jpg" alt="Cena CoSpaces" width="100%">
+          </div>
+        </div>
       </div>
-      <div id="item-3">
-        <h3>Item 3</h3>
-        <h4>...</h4>
+      <div class="item-3">
+        <div class="row">
+          <h3>Rotação - Giro</h3>
+          <h4>Para Usos de Desktop e Web</h4>
+          <h4>A rotação ou giro em 360° 3D é mediada pela navegação de deslize do curor na tela</h4>
+          <img src="imagens/tutorial/navega_web.png" alt="Cena do CoSpaces" width="100%">
+        </div>
       </div>
-      <div id="item-3-1">
-        <h5>Item 3-1</h5>
-        <h4>...</h4>
+      <div id="item-4">
+        <h3>Rotação - Giro</h3>
+        <h4>Para Usos de Aplicativo Móvel</h4>
+        <h4>A rotação ou giro em 360° 3D é variável conforme o modo de execução</h4>
+        <div class="row">
+          <div class="col-12">
+            <img src="imagens/tutorial/rotacao.jpeg" alt="Cena do CoSpaces" width="100%">
+          </div>
+        </div>
       </div>
-      <div id="item-3-2">
-        <h5>Item 3-2</h5>
-        <h4>...</h4>
+      <div id="item-4-1">
+        <div class="row">
+          <div class="col-4">
+          <h5>Quando em R.V (Realidade Virtual), a rotação 
+            é mediada pela sensibilidade de rotação do próprio aparelho eletrônico
+          </h5>
+          <br>
+          <h5>
+            Aqui temos um exemplo de visão em R.V, no qual, a tela é dividida especificamente para a visão com óculos
+          </h5>
+          <h5 id="rv">
+            Destacado na cor azul, observamos a seleção necessária para ver no modo R.V
+          </h5>
+          </div>
+          <div class="col-8">
+            <img src="imagens/tutorial/rv.jpeg" alt="Cena do CoSpaces" width="100%">
+            <img src="imagens/tutorial/rv2.jpg" alt="Cena do CoSpaces" width="100%">
+          </div>
+        </div>
+        
+      </div>
+      <div id="item-4-2">
+        <h4 id="giro">Quando em R.A (Realidade Aumentada), a rotação varia caso o giroscópio esteja ligado</h4>
+        <h5>Abaixo, vemos a opção a ser selecionada para ligar o giroscópio</h5>
+        <img src="imagens/tutorial/giro.jpg" alt="Cena do Cospaces" width="100%">
+      </div>
+      <div id="item-4-3">
+        <div class="row">
+          <div class="col-4">
+            <h5>No caso de Giroscópio desligado, a rotação é mediada pelo deslize na tela do aparelho móvel</h5>  
+          </div>
+          <div class="col-8">
+            <img src="imagens/tutorial/ra.jpg" alt="Cena CoSpaces" width="100%">
+          </div>
+        </div>
       </div>
     </div>
   </div>
